55d8f672252a5bf64535791470d1a723b1dea678
kd9397 <kd9397@DESKTOP-VK3SSIA>
2023-09-21 10:45:43 +0900

    수정

--- Homework/homework.c
+++ Homework/homework.c
@@ -1,111 +1,71 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
-#define SIZE 100
+#define TRUE 1
+#define FALSE 0
 
 typedef struct TreeNode {
     int data;
     struct TreeNode* left, * right;
+    int is_thread;
 } TreeNode;
 
-int top = -1;
-TreeNode* stack[SIZE];
 
-void push(TreeNode* p)
+
+TreeNode s1 = { 4, NULL,NULL,1};
+TreeNode s2 = { 5, NULL,NULL,1};
+TreeNode s3 = { 3, &s1,&s2,0};
+TreeNode s4 = { 6, NULL,NULL,1 };
+TreeNode s5 = { 2, &s3,&s4,0 };
+TreeNode s6 = { 10, NULL,NULL,1 };
+TreeNode s7 = { 11, NULL,NULL,0 };
+TreeNode s8 = { 8, NULL,NULL,1 };
+TreeNode s9 = { 9, &s6,&s7,0 };
+TreeNode s10 = { 7, &s8,&s9,0 };
+TreeNode s11 = { 1, &s5,&s10,0 };
+TreeNode* exp = &s11;
+
+
+
+TreeNode* find_successor(TreeNode* p)
 {
-    if (top < SIZE - 1)
-        stack[++top] = p;
+    TreeNode* q = p->right; //q=p의 오른쪽 포인터
+
+    // 만약 오른쪽 포인터가 NULL이거나 스레드이면 오른쪽 포인터 반환
+    if (q == NULL || p->is_thread == TRUE)
+        return q;
+
+    //만약 오른쪽 자식이면 다시 가장 왼쪽 노드 이동
+    while (q->left != NULL) q = q->left;
+    return q;
 }
 
-TreeNode* pop()
+void thread_inorder(TreeNode* t)
 {
-    TreeNode* p = NULL;
-    if (top >= 0)
-        p = stack[top--];
-    return p;
-}
-
-int get_node_count_iter(TreeNode* root)
-{
-    int count = 0;
-    if (root == NULL)
-        return count;
-
-    TreeNode* stack[100]; // 스택을 사용하여 노드를 저장
-    int top = -1;
-
-    stack[++top] = root; // 루트 노드를 스택에 추가
-
-    while (top >= 0) {
-        TreeNode* current = stack[top--]; // 스택에서 노드를 꺼내옴
-        count++; // 노드 카운트 증가
-
-        // 오른쪽 자식을 먼저 스택에 추가
-        if (current->right)
-            stack[++top] = current->right;
-
-        // 왼쪽 자식을 스택에 추가
-        if (current->left)
-            stack[++top] = current->left;
-    }
-
-    return count;
+    TreeNode* q;
+    q = t;
+    while (q->left)q = q->left;//가장 왼쪽 노드로 감
+    do {
+        printf("%d ", q->data); //데이터 출력
+        q = find_successor(q); //후속자 함수 호출
+    } while (q);
 }
 
 
 
 
-TreeNode s1 = { 4.0, NULL,NULL };
-TreeNode s2 = { 5.0, NULL,NULL };
-TreeNode s3 = { '*',&s1,&s2 };
-TreeNode s4 = { 3.0, NULL,NULL };
-TreeNode s5 = { '+', &s4,&s3 };
-TreeNode s6 = { 2.0, NULL,NULL };
-TreeNode s7 = { '+', &s6,&s5 };
-TreeNode s8 = { 6.0, NULL,NULL };
-TreeNode s9 = { 7.0, NULL,NULL };
-TreeNode s10 = { '/', &s8,&s9 };
-TreeNode s11 = { '-', &s7,&s10 };
-TreeNode s12 = { 9.0, NULL,NULL };
-TreeNode s13 = { '+', &s11,&s12 };
-TreeNode* exp = &s13;
-
-float evaluate(TreeNode* root)
-{
-
-    if (root == NULL)
-        return 0;
-    if (root->left == NULL && root->right == NULL)
-        return root->data;
-    else {
-        float op1 = evaluate(root->left);
-        float op2 = evaluate(root->right);
-        printf("%0.2f %c %0.2f = ", op1, root->data, op2);
-
-        switch (root->data) {
-        case '+':
-            printf("%0.2f\n", op1 + op2);
-            return op1 + op2;
-        case '-':
-            printf("%0.2f\n", op1 - op2);
-            return op1 - op2;
-        case '*':
-            printf("%0.2f\n", op1 * op2);
-            return op1 * op2;
-        case '/':
-            printf("%0.2f\n", op1 / op2);
-            return op1 / op2;
-        }
-
-    }
-    return 0;
-}
 int main(void)
 {
+    //스레드 설정
+    s1.right = &s3;
+    s2.right = &s5;
+    s4.right = &s11;
+    s6.right = &s9;
+    s8.right = &s10;
+    
+    printf("1. 중위 순회 결과\n");
+    thread_inorder(exp);
+    printf("\n\n");
 
-    printf("수식의 값은 %0.2f입니다.\n", evaluate(exp));
 
-    int nodeCount = get_node_count_iter(exp); // 반복적 방법으로 노드의 개수 계산
-    printf("\n총 노드의 개수는 %d 입니다.\n", nodeCount);
+
     return 0;
 }
