4bf0042f70d65856124d95b9031c63d197a344b5
kd9397 <kd9397@DESKTOP-VK3SSIA>
2023-10-31 10:43:18 +0900

    수정

--- Homework/homework.c
+++ Homework/homework.c
@@ -1,111 +1,177 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
-#define SIZE 100
 
-typedef struct TreeNode {
-    int data;
-    struct TreeNode* left, * right;
-} TreeNode;
+#define TRUE 1
+#define FALSE 0
+#define MAX 10
 
-int top = -1;
-TreeNode* stack[SIZE];
+typedef int element;
+typedef struct {
+    int front;
+    int rear;
+    element data[MAX];
+}QueueType;
 
-void push(TreeNode* p)
+void error(char* message)
 {
-    if (top < SIZE - 1)
-        stack[++top] = p;
+    fprintf(stderr, "%s\n", message);
+
 }
 
-TreeNode* pop()
+void init_queue(QueueType* q)
 {
-    TreeNode* p = NULL;
-    if (top >= 0)
-        p = stack[top--];
-    return p;
+    q->front = q->rear = 0;
 }
 
-int get_node_count_iter(TreeNode* root)
+
+
+int is_full(QueueType* q)
 {
-    int count = 0;
-    if (root == NULL)
-        return count;
+    return((q->rear + 1) % MAX == q->front);
+}
+int is_empty(QueueType* q)
+{
+    return(q->front == q->rear);
 
-    TreeNode* stack[100]; // 스택을 사용하여 노드를 저장
-    int top = -1;
-
-    stack[++top] = root; // 루트 노드를 스택에 추가
-
-    while (top >= 0) {
-        TreeNode* current = stack[top--]; // 스택에서 노드를 꺼내옴
-        count++; // 노드 카운트 증가
-
-        // 오른쪽 자식을 먼저 스택에 추가
-        if (current->right)
-            stack[++top] = current->right;
-
-        // 왼쪽 자식을 스택에 추가
-        if (current->left)
-            stack[++top] = current->left;
+}
+void enqueue(QueueType* q, int item)
+{
+    if (is_full(q)) {
+        error("큐가 포화상태");
     }
+    q->rear = (q->rear + 1) % MAX;
+    q->data[(q->rear)] = item;
+}
+element dequeue(QueueType* q)
+{
+    if (is_empty(q))
+    {
+        error("큐 공백상태");
+    }
+    q->front = (q->front + 1) % MAX;
+    return q->data[q->front];
 
-    return count;
+}
+#define MAX_VERTICES 50
+
+typedef struct GrapNode {
+    int vertex;
+    struct GraphNode* link;
+}GraphNode;
+typedef struct GraphType {
+    int n;
+    int adj_mat[MAX_VERTICES][MAX_VERTICES];
+}GraphType;
+
+int visited[MAX_VERTICES];
+void init(GraphType* g) {
+    int r, c;
+    g->n = 0;
+    for (r = 0; r < MAX_VERTICES; r++)
+        for (c = 0; c < MAX_VERTICES; c++)
+            g->adj_mat[r][c] = 0;
 }
 
-
-
-
-TreeNode s1 = { 4.0, NULL,NULL };
-TreeNode s2 = { 5.0, NULL,NULL };
-TreeNode s3 = { '*',&s1,&s2 };
-TreeNode s4 = { 3.0, NULL,NULL };
-TreeNode s5 = { '+', &s4,&s3 };
-TreeNode s6 = { 2.0, NULL,NULL };
-TreeNode s7 = { '+', &s6,&s5 };
-TreeNode s8 = { 6.0, NULL,NULL };
-TreeNode s9 = { 7.0, NULL,NULL };
-TreeNode s10 = { '/', &s8,&s9 };
-TreeNode s11 = { '-', &s7,&s10 };
-TreeNode s12 = { 9.0, NULL,NULL };
-TreeNode s13 = { '+', &s11,&s12 };
-TreeNode* exp = &s13;
-
-float evaluate(TreeNode* root)
+void insert_vertex(GraphType* g, int v)
 {
+    if (((g->n) + 1) > MAX_VERTICES) {
+        fprintf(stderr, "그래프 : 정점의 개수 초과");
+        return;
+    }
+    g->n++;
+}
+void insert_edge(GraphType* g, int start, int end)
+{
+    if (start >= g->n || end >= g->n) {
+        fprintf(stderr, "그래프 : 정점 번호 오류");
+        return;
+    }
+    g->adj_mat[start][end] = 1;
+    g->adj_mat[end][start] = 1;
+}
 
-    if (root == NULL)
-        return 0;
-    if (root->left == NULL && root->right == NULL)
-        return root->data;
-    else {
-        float op1 = evaluate(root->left);
-        float op2 = evaluate(root->right);
-        printf("%0.2f %c %0.2f = ", op1, root->data, op2);
+void dfs_mat(GraphType* g, int v)
+{
+    int w;
+    visited[v] = TRUE;
+    printf("%d ", v);
+    for (w = 0; w < g->n; w++)
+        if (g->adj_mat[v][w] && !visited[w])
+            dfs_mat(g, w);
+}
 
-        switch (root->data) {
-        case '+':
-            printf("%0.2f\n", op1 + op2);
-            return op1 + op2;
-        case '-':
-            printf("%0.2f\n", op1 - op2);
-            return op1 - op2;
-        case '*':
-            printf("%0.2f\n", op1 * op2);
-            return op1 * op2;
-        case '/':
-            printf("%0.2f\n", op1 / op2);
-            return op1 / op2;
+void bfs_mat(GraphType* g, int v)
+{
+    int w;
+    QueueType q;
+    init_queue(&q);
+    visited[v] = TRUE;
+    printf("%d ", v);
+    enqueue(&q, v);
+    while (!is_empty(&q)) {
+        v = dequeue(&q);
+        for (w =0; w<g->n; w++)
+            if (g->adj_mat[v][w]&&!visited[w])
+            {
+                visited[w] = TRUE;
+                printf("%d ", w);
+                enqueue(&q, w);
+            }
+    }
+}
+
+void main()
+{
+    GraphType* g;
+    g = (GraphType*)malloc(sizeof(GraphType));
+    init(g);
+    for (int i = 0; i < 10; i++)
+        insert_vertex(g, i);
+    insert_edge(g, 0, 4);
+    
+    insert_edge(g, 1, 5);
+    insert_edge(g, 1, 4);
+    insert_edge(g, 1, 7);
+    insert_edge(g, 1, 10);
+    insert_edge(g, 2, 3);
+    insert_edge(g, 4, 0);
+    insert_edge(g, 4, 2);
+    insert_edge(g, 4, 3);
+    insert_edge(g, 4, 5);
+    insert_edge(g, 4, 6);
+    insert_edge(g, 4, 7);
+    insert_edge(g, 4, 6);
+    insert_edge(g, 4, 6); 
+    insert_edge(g, 5, 0);
+    insert_edge(g, 5, 3);
+    insert_edge(g, 6, 0);
+    insert_edge(g, 6, 8);
+    insert_edge(g, 7, 6);
+    insert_edge(g, 7, 10);
+    insert_edge(g, 10, 8);
+    insert_edge(g, 8, 9);
+    insert_edge(g, 0, 9);
+    printf("-----------------------\n");
+    printf("1    : 깊이 우선 탐색  |\n");
+    printf("2    : 너비 우선 탐색  |\n");
+    printf("3    : 종료            |\n");
+    printf("-----------------------\n");
+    int n,start,search;
+    
+    while (1) {
+        printf("메뉴 입력: ");
+        scanf_s("%d", &n);
+        printf("\n");
+        if (n == 1)
+        {
+            printf("시작 할 번호와 탐색할 값 입력: ");
+            scanf_s("%d %d", &start, &search);
+            dfs_mat(g, start);
+            printf("\n");
         }
-
     }
-    return 0;
-}
-int main(void)
-{
-
-    printf("수식의 값은 %0.2f입니다.\n", evaluate(exp));
-
-    int nodeCount = get_node_count_iter(exp); // 반복적 방법으로 노드의 개수 계산
-    printf("\n총 노드의 개수는 %d 입니다.\n", nodeCount);
+    
+    free(g);
     return 0;
 }
