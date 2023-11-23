4def80cf069723b73520d487b47bf9fe21639e41
kd9397 <kd9397@DESKTOP-VK3SSIA>
2023-11-23 11:19:53 +0900

    수정

--- Homework/homework.c
+++ Homework/homework.c
@@ -4,50 +4,38 @@
 #define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))
 
 int list[MAX];
-int n,cnt1,cnt2;
+int n,cnt1,cnt2,round;
+int sum1 = 0;
+int sum2 = 0;
 int average_compare_cnt = 0;
 int average_move_cnt = 0;
 //선택 정렬
 void selection_sort(int list[], int n)
 {
-	int sum1 = 0;
-	int sum2 = 0;
+
 
 	int i, j,k,l, least, temp;
-	//20번 반복
-	for (l = 0; l < MAX; l++) {
-		cnt1 = 0;
-		cnt2 = 0;
-		for (i = 0; i < n - 1; i++) {
-			least = i;
 
-			for (j = i + 1; j < n; j++) {
-				cnt2++;
-				if (list[j] < list[least])
-				{
-					least = j;
-
-				}
-			}
-			SWAP(list[i], list[least], temp);
-			cnt1++;
-			if (l == 0)
+	for (i = 0; i < n - 1; i++) {
+		least = i;
+		for (j = i + 1; j < n; j++) {
+			cnt2++;
+			if (list[j] < list[least])
 			{
-				for (k = 0; k < n; k++) {
-					printf("%d ", list[k]);
-				}
-				printf("\n");
+				least = j;
 
 			}
 		}
-		sum1 += cnt1;
-		sum2 += cnt2;
-
+		SWAP(list[i], list[least], temp);
+		cnt1++;
+		if (round == 0)
+		{
+			for (k = 0; k < n; k++) {
+				printf("%d ", list[k]);
+			}
+			printf("\n");
+		}
 	}
-	average_compare_cnt = sum2 / 20;
-	average_move_cnt = sum1 / 20;
-
-
 }
 // 삽입 정렬 미완성
 void insertion_sort(int list[], int n) {
@@ -59,10 +47,12 @@ void insertion_sort(int list[], int n) {
 			list[j + 1] = list[j];
 			cnt2++;
 		}
-		for (k = 0; k < n; k++) {
-			printf("%d ", list[k]);
+		if (round == 0) {
+			for (k = 0; k < n; k++) {
+				printf("%d ", list[k]);
+			}
+			printf("\n");
 		}
-		printf("\n");
 		list[j + 1] = key;
 		cnt1++;
 	}
@@ -75,8 +65,15 @@ void bubble_sort(int list[],int n){
 			if (list[j] < list[j+1])
 			{
 				SWAP(list[j], list[j + 1], temp);
-	
+				cnt2++;
 			}
+		if (round == 0) {
+			for (int k = 0; k < n; k++) {
+				printf("%d ", list[k]);
+			}
+			printf("\n");
+		}
+		cnt1++;
 	}
 }
 
@@ -85,11 +82,21 @@ int main(void)
 	int i;
 	n = MAX;
 	srand(time(NULL));
-	for (i = 0; i < n; i++) {
-		list[i] = rand() % 100;
+	printf("\n Selection sort\n\n");
+	for (round = 0; round < n; round++) {
+		cnt1 = 0;
+		cnt2 = 0;
+		for (i = 0; i < n; i++) {
+			list[i] = rand() % 100;
+
+		}
+
+		selection_sort(list, n);
+		sum1 += cnt1;
+		sum2 += cnt2;
 	}
-	
-	selection_sort(list, n);
+	average_compare_cnt = sum2/MAX;
+	average_move_cnt = (sum1-1)/MAX;
 
 	printf("Move Count: %d\n",cnt1);
 	printf("Compare Count: %d", cnt2);
