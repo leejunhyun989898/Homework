a7b4a1e0a03d1ee818ab95ec2eb8fd5c235b1be4
kd9397 <kd9397@DESKTOP-VK3SSIA>
2023-11-23 10:40:30 +0900

    수정

--- Homework/homework.c
+++ Homework/homework.c
@@ -3,97 +3,82 @@
 #define MAX 20
 #define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))
 
+int sorted[MAX];
 int list[MAX];
-int n,cnt1,cnt2;
+int n,cnt1,cnt2,sum1,sum2,round;
 int average_compare_cnt = 0;
 int average_move_cnt = 0;
-//선택 정렬
-void selection_sort(int list[], int n)
-{
-	int sum1 = 0;
-	int sum2 = 0;
-
-	int i, j,k,l, least, temp;
-	//20번 반복
-	for (l = 0; l < MAX; l++) {
-		cnt1 = 0;
-		cnt2 = 0;
-		for (i = 0; i < n - 1; i++) {
-			least = i;
-
-			for (j = i + 1; j < n; j++) {
-				cnt2++;
-				if (list[j] < list[least])
-				{
-					least = j;
-
-				}
-			}
-			SWAP(list[i], list[least], temp);
-			cnt1++;
-			if (l == 0)
-			{
-				for (k = 0; k < n; k++) {
-					printf("%d ", list[k]);
-				}
-				printf("\n");
-
-			}
-		}
-		sum1 += cnt1;
-		sum2 += cnt2;
-
-	}
-	average_compare_cnt = sum2 / 20;
-	average_move_cnt = sum1 / 20;
-
-
-}
-// 삽입 정렬 미완성
-void insertion_sort(int list[], int n) {
-	int i,j,k,key;
-	for (i = 1; i < n; i++) {
+//gap 만큼 떨어진 요소들을 삽입 정렬
+// 정렬의 범위 first~last
+inc_insertion_sort(int list[], int first, int last, int gap) {
+	int i, j, key;
+	for (i = first + gap; i <= last; i = i + gap) {
 		key = list[i];
-		for (j = i - 1; j >= 0 && list[j] > key; j--)
-		{
-			list[j + 1] = list[j];
-			cnt2++;
+		for (j = i - gap; j >= first && key < list[j]; j = j - gap) {
+			list[j + gap] = list[j];
+			cnt1++;
 		}
-		for (k = 0; k < n; k++) {
-			printf("%d ", list[k]);
-		}
-		printf("\n");
-		list[j + 1] = key;
-		cnt1++;
+		list[j + gap] = key;
+		cnt2++;
 	}
 }
-//버블정렬 미구현
-void bubble_sort(int list[],int n){
-	int i, j, temp;
-	for (i = n-1; i >0; i--) {
-		for (j = 0;j<i; j++)
-			if (list[j] < list[j+1])
-			{
-				SWAP(list[j], list[j + 1], temp);
-	
+
+void shell_sort(int list[], int n) {
+	int i, gap;
+	for (gap = n / 2; gap > 0; gap = gap / 2) {
+		if ((gap % 2) == 0)gap++;
+		for (i = 0; i < gap; i++) //부분리스트의 개수= gap
+		{
+			inc_insertion_sort(list, i, n - 1, gap);
+		}
+		if (round == 0)
+		{
+			for (int k = 0; k < n; k++) {
+				printf("%d ", list[k]);
 			}
+			printf("\n");
+		}
 	}
+	sum1 += cnt1;
+	sum2 += cnt2;
 }
 
 int main(void)
 {
 	int i;
 	n = MAX;
+	int before_list[MAX];
 	srand(time(NULL));
-	for (i = 0; i < n; i++) {
-		list[i] = rand() % 100;
-	}
-	
-	selection_sort(list, n);
 
-	printf("Move Count: %d\n",cnt1);
-	printf("Compare Count: %d", cnt2);
-	printf("\n\nMove Count average: %d\n", average_move_cnt);
+	for (round = 0; round < n; round++) {
+		cnt1 = 0;
+		for (i = 0; i < n; i++) {
+			list[i] = rand() % 100;
+			if (round == 0) {
+				before_list[i] = list[i];
+			}
+
+		}
+
+	}
+
+	printf("Original list: ");
+	for (round = 0; round < n; round++) {
+		printf("%d ", before_list[round]);
+	}
+	printf("\n\n");
+	for (round = 0; round < n; round++) {
+		
+		cnt2 = 0;
+
+		shell_sort(list, n);
+	}
+
+	printf("\n");
+	average_move_cnt = sum2/MAX;
+	average_compare_cnt = sum1/MAX;
+	
+	printf("\nMove Count average: %d\n", average_move_cnt-1);
 	printf("Compare Count average: %d\n", average_compare_cnt);
 	return 0;
 }
